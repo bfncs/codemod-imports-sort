42ec032b3392b3157cc70dfd044175a6868e0d5b
Marc Löhe <marc@marcloehe.de>
2017-04-08 15:10:32 +0200

    Add simple integration test

---
+++ src/index.test.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import jscodeshift from 'jscodeshift';
+import testCodemod from 'jscodeshift-ava-tester';
+import sortImports from './';
+
+const { testChanged, testUnchanged } = testCodemod(jscodeshift, test, sortImports);
+
+testChanged(
+  "sort imports",
+  `import foo from './foo'; import fs from 'fs'; let foo = 'bar';`,
+  `import fs from 'fs'; import foo from './foo'; let foo = 'bar';`,
+);
+
+testUnchanged(`let foo = 'bar';`);
