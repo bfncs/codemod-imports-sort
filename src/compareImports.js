c0d81391401f0f77539662464116d293cb91aa59
Marc Löhe <marc@marcloehe.de>
2017-05-19 14:14:07 +0200

    Reimplement compareImports with optional configurable order

--- src/compareImports.js
+++ src/compareImports.js
@@ -1,7 +1,17 @@
 import isBuiltinModule from 'is-builtin-module';
 
+const DEFAULT_ORDER = [
+  'builtin',
+  'external',
+  'internal',
+  'parent',
+  'sibling',
+  'index',
+];
 const CURRENT_DIRECTORY_PREFIX = './';
 const PARENT_DIRECTORY_PREFIX = '../';
+
+const concat = (x, y) => x.concat(y);
 
 export const isExternalModule = path =>
   /^@?[\w-]+$/.test(path) && !isBuiltinModule(path);
@@ -14,30 +24,81 @@ export const isLocalModuleFromSiblingDirectory = path =>
   !isLocalModuleCurrentDirectoryIndex(path) &&
   path.startsWith(CURRENT_DIRECTORY_PREFIX);
 
-export const compareByMatch = (match, a, b) => {
-  const aIsMatching = match(a);
-  if (aIsMatching === match(b)) {
+const matchers = {
+  builtin: isBuiltinModule,
+  external: isExternalModule,
+  internal: isInternalModule,
+  parent: isLocalModuleFromParentDirectory,
+  index: isLocalModuleCurrentDirectoryIndex,
+  sibling: isLocalModuleFromSiblingDirectory,
+};
+const matcherNames = Object.keys(matchers);
+
+export const compareByMatcher = (matcher, a, b) => {
+  const aIsMatching = matcher(a);
+  if (aIsMatching === matcher(b)) {
     return null;
   }
   return aIsMatching ? -1 : 1;
 };
 
-export default (a, b) => {
-  const importTypeMatchFunctions = [
-    isBuiltinModule,
-    isExternalModule,
-    isInternalModule,
-    isLocalModuleFromParentDirectory,
-    isLocalModuleCurrentDirectoryIndex,
-    isLocalModuleFromSiblingDirectory,
-  ];
+const isValidSorterName = (name, warn = true) => {
+  const isValid = matcherNames.includes(name);
+  if (warn && !isValid) {
+    console.warn(
+      `You used an invalid import sort group: "${name}". Check your configuration.`,
+    );
+  }
+  return isValid;
+};
 
-  const result = importTypeMatchFunctions.reduce(
-    (acc, match) => {
-      return acc === null ? compareByMatch(match, a, b) : acc;
+export const sanitizeOrder = order => {
+  const bins = order.reduce(
+    (acc, item) => {
+      if (Array.isArray(item)) {
+        const group = item.filter(isValidSorterName).sort();
+        if (!group.length) {
+          return acc;
+        }
+        return [...acc, group];
+      }
+      if (isValidSorterName(item)) {
+        return [...acc, item];
+      }
+      return acc;
     },
-    null,
+    [],
   );
+  const usedSorters = bins.reduce(concat, []);
+  const omittedSorters = matcherNames.filter(
+    name => !usedSorters.includes(name),
+  );
+  if (omittedSorters.length) {
+    bins.push(
+      omittedSorters.length === 1 ? omittedSorters[0] : omittedSorters.sort(),
+    );
+  }
+  return bins;
+};
 
+export default (a, b, order = DEFAULT_ORDER) => {
+  const result = sanitizeOrder(order)
+    .map(
+      group =>
+        Array.isArray(group)
+          ? name =>
+              group.reduce(
+                (acc, matcherName) =>
+                  acc === null ? matchers[matcherName] : acc,
+                null,
+              )
+          : matchers[group],
+    )
+    .reduce(
+      (acc, matcher) => {
+        return acc === null ? compareByMatcher(matcher, a, b) : acc;
+      },
+      null,
+    );
   return result !== null ? result : a.localeCompare(b);
 };
--- src/compareImports.test.js
+++ src/compareImports.test.js
@@ -1,12 +1,37 @@
 import test from 'ava';
 import compareImports, {
+  sanitizeOrder,
   isExternalModule,
   isInternalModule,
   isLocalModuleFromParentDirectory,
   isLocalModuleCurrentDirectoryIndex,
   isLocalModuleFromSiblingDirectory,
-  compareByMatch,
+  compareByMatcher,
 } from './compareImports';
+
+const DEFAULT_ORDER = [
+  'builtin',
+  'external',
+  'internal',
+  'parent',
+  'sibling',
+  'index',
+];
+
+test('sanitizeOrder should leave default order unchanged', t => {
+  t.deepEqual(sanitizeOrder(DEFAULT_ORDER), DEFAULT_ORDER);
+});
+
+test('sanitizeOrder should append all omitted groups', t => {
+  t.deepEqual(sanitizeOrder(['external']), [
+    'external',
+    ['builtin', 'internal', 'parent', 'sibling', 'index'].sort(),
+  ]);
+  t.deepEqual(
+    sanitizeOrder(['builtin', ['internal', 'parent', 'sibling', 'index']]),
+    ['builtin', ['internal', 'parent', 'sibling', 'index'].sort(), 'external'],
+  );
+});
 
 const BUILTIN_MODULE_A = 'fs';
 const BUILTIN_MODULE_B = 'http';
@@ -101,17 +126,17 @@ test('isLocalModuleFromSiblingDirectory is true only for current directory index
 
 const matchIfIsA = test => test === 'a';
 
-test('compareByMatch favors matching', t => {
-  t.is(compareByMatch(matchIfIsA, 'a', 'b'), FIRST);
-  t.is(compareByMatch(matchIfIsA, 'b', 'a'), SECOND);
+test('compareByMatcher favors matching', t => {
+  t.is(compareByMatcher(matchIfIsA, 'a', 'b'), FIRST);
+  t.is(compareByMatcher(matchIfIsA, 'b', 'a'), SECOND);
 });
 
-test('compareByMatch returns null if none is matching', t => {
-  t.is(compareByMatch(matchIfIsA, 'b', 'c'), null);
+test('compareByMatcher returns null if none is matching', t => {
+  t.is(compareByMatcher(matchIfIsA, 'b', 'c'), null);
 });
 
-test('compareByMatch returns null if both are matching', t => {
-  t.is(compareByMatch(matchIfIsA, 'a', 'a'), null);
+test('compareByMatcher returns null if both are matching', t => {
+  t.is(compareByMatcher(matchIfIsA, 'a', 'a'), null);
 });
 
 test('compareImports favors builtin modules before all others', t => {
@@ -158,9 +183,9 @@ test('compareImports favors parent dir modules before other local modules', t =>
   t.is(compareImports(LOCAL_SIBLING_MODULE_A, LOCAL_PARENT_MODULE_A), SECOND);
 });
 
-test('compareImports favors local index modules before other sibling modules', t => {
-  t.is(compareImports(LOCAL_INDEX_MODULE, LOCAL_SIBLING_MODULE_A), FIRST);
-  t.is(compareImports(LOCAL_SIBLING_MODULE_A, LOCAL_INDEX_MODULE), SECOND);
+test('compareImports favors sibling modules before local index modules', t => {
+  t.is(compareImports(LOCAL_SIBLING_MODULE_A, LOCAL_INDEX_MODULE), FIRST);
+  t.is(compareImports(LOCAL_INDEX_MODULE, LOCAL_SIBLING_MODULE_A), SECOND);
 });
 
 test('compareImports sorts imports of the same type alphabetically', t => {
