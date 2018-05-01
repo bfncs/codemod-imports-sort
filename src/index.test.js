8ae38102c1121828739fb86be280127951be8650
Sang Mercado <smercado@linkedin.com>
2018-05-01 12:23:20 -0700

    Ran prettier

--- src/index.test.js
+++ src/index.test.js
@@ -6,13 +6,13 @@ import sortImports from './';
 const { testChanged, testUnchanged } = testCodemod(
   jscodeshift,
   test,
-  sortImports,
+  sortImports
 );
 
 testChanged(
   'sort imports',
   `import foo from './foo'; import fs from 'fs'; let foo = 'bar';`,
-  `import fs from 'fs'; import foo from './foo'; let foo = 'bar';`,
+  `import fs from 'fs'; import foo from './foo'; let foo = 'bar';`
 );
 
 testUnchanged(`let foo = 'bar';`);
